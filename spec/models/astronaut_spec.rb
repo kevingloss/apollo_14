f8ea44a654945ed4ff13459898c89aaa117f1fde
Kevin Gloss <kgloss89@gmail.com>
2021-12-21 10:22:11 -0700

    Add average age to astronaut

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,14 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  before :each do
+    @astro = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
+    @astro_2 = Astronaut.create!(name: 'Amelia Airheart', age: 45, job: 'Pilot')
+    @astro_3 = Astronaut.create!(name: 'Gordon Ramsey', age: 32, job: 'Chef')
+  end
+
+  it 'can calculate the average age of astronauts' do
+    expect(Astronaut.average_age).to eq((@astro.age + @astro_2.age + @astro_3.age)/Astronaut.count)
+  end
 end
